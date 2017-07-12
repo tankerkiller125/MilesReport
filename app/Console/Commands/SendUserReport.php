0bf7945a8b5f7fb7ab7305e5262c2f516f426d93
tankerkiller125 <tankerkiller125@gmail.com>
2017-07-12 13:28:26 -0400

    Update some tiny changes. Good to go for super basic release.

--- app/Console/Commands/SendUserReport.php
+++ app/Console/Commands/SendUserReport.php
@@ -18,7 +18,7 @@ class SendUserReport extends Command
      *
      * @var string
      */
-    protected $signature = 'milesreport:send-user-report {user?}';
+    protected $signature = 'milesreport:send-user-report {user?} {--force}';
 
     /**
      * The console command description.
@@ -54,29 +54,32 @@ class SendUserReport extends Command
                         // Get all entries from last report sent to now
                         $sheet = $this->generateSheet($user->id, $user->name, $user->last_report);
                         $user->notify(new Notification($sheet));
+                        $user->last_report = Carbon::now();
+                        $user->save();
                     } else {
                         $this->info('Report not generated for ' . $user->name);
                     }
                 }
                 $this->info('Reports generated and sent!');
             } catch (\Exception $e) {
-                $this->error($e->getTraceAsString());
+                $this->error($e->getMessage());
             }
         } else {
             try {
                 $user = User::whereId($this->argument('user'))->first();
-
                 // This crazy compare determines if the user wants the message sent today or if its too soon
-                if (Carbon::createFromFormat('Y-m-d H:i:s', $user->last_report)->addDays($user->report_schedule)->diffInDays(Carbon::now()) == 0) {
+                if (Carbon::createFromFormat('Y-m-d H:i:s', $user->last_report)->addDays($user->report_schedule)->diffInDays(Carbon::now()) == 0 || $this->option('force')) {
                     // Get all entries from last report sent to now
                     $sheet = $this->generateSheet($user->id, $user->name, $user->last_report);
                     $user->notify(new Notification($sheet));
+                    $user->last_report = Carbon::now();
+                    $user->save();
                     $this->info('Report generated!');
                 } else {
                     $this->info('Report not generated for ' . $user->name);
                 }
             } catch (\Exception $e) {
-                $this->error($e->getTraceAsString());
+                $this->error($e->getMessage());
             }
         }
     }
@@ -88,8 +91,12 @@ class SendUserReport extends Command
      * @return string
      */
     private function generateSheet($userId, $userName, $lastReport) {
-        $entries = Entry::whereUserId($userId)->whereDate('created_at', '>', $lastReport);
-        $sheet = \Excel::create('Miles-Report-' . str_replace(' ', '-', $userName), function ($excel) use($entries) {
+        $entries = Entry::whereUserId($userId)->whereDate('created_at', '>', $lastReport)->get()->map(function($entry) {
+            $from = Location::whereId($entry->from)->first();
+            $to = Location::whereId($entry->to)->first();
+            return ['from' => $from->name, 'to' => $to->name, 'distance' => $entry->distance];
+        });
+        $sheet = \Excel::create('Miles-Report-' . str_replace(' ', '-', $userName). '-' . Carbon::now()->format('F') . '-' . Carbon::now()->format('Y'), function ($excel) use($entries) {
             $excel->sheet('Miles', function($sheet) use($entries) {
                 $sheet->fromModel($entries);
             });
--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@milesreport.tld'),
+        'name' => env('MAIL_FROM_NAME', 'No-Reply Miles Report'),
     ],
 
     /*
