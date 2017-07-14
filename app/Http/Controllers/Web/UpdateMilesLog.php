aea1380952a0bc308746a68f697e464102f9b6db
tankerkiller125 <tankerkiller125@gmail.com>
2017-07-14 01:30:23 -0400

    🐛 Needs to update the distance and time too

--- app/Http/Controllers/Web/UpdateMilesLog.php
+++ app/Http/Controllers/Web/UpdateMilesLog.php
@@ -41,8 +41,11 @@ class UpdateMilesLog extends Controller
                     'mpg' => ['nullable', 'integer'],
                     'created_at' => ['required', 'date_format:"Y-m-d H:i:s"']
                 ]);
+                $distance = $this->getDistance(Location::whereId($request->input('from')), Location::whereId($request->input('to')));
                 $entry->from = $request->input('from');
                 $entry->to = $request->input('to');
+                $entry->distance = $distance->distance;
+                $entry->time = $distance->time;
                 $entry->mpg = $request->input('mpg');
                 $entry->created_at = $request->input('created_at');
                 $entry->save();
@@ -53,5 +56,31 @@ class UpdateMilesLog extends Controller
         } else {
             return redirect()->back()->withErrors(['error' => 'You do not have access to do that']);
         }
+    }
+
+    /**
+     * Get distance and time from Google Distance Matrix API
+     *
+     * @param Location $origin
+     * @param Location $destination
+     * @return object
+     */
+    public function getDistance(Location $origin, Location $destination)
+    {
+        $httpClient = new HttpClient();
+        $response = $httpClient->get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' . $origin->address . '&destinations=' . $destination->address . '&key=' . config('services.google.key'));
+        if ($response->getStatusCode() < 200 || $response->getStatusCode() >= 400) {
+            // Return an error if we can not access google
+            return abort(500, 'We could not get the distance information from google. Did you put your google API key in the config? And did you enable the Distance Matrix API?');
+        }
+        // This is the raw data from Google converted to json
+        $rawData = (object)\GuzzleHttp\json_decode($response->getBody());
+
+        // This is the raw miles (meters to miles)
+        $cleanDistance = $rawData->rows[0]->elements[0]->distance->value / 1609.344;
+        // This is the seconds converted to minutes
+        $cleanTime = $rawData->rows[0]->elements[0]->duration->value / 60;
+        // Return the distance value
+        return (object)['distance' => (float)substr($cleanDistance, 0, 4), 'time' => (integer)$cleanTime];
     }
 }
