e6a91965ae7070eef54047fbb8ff0d6e7de204dc
tankerkiller125 <tankerkiller125@gmail.com>
2017-07-24 11:42:32 -0400

    :sparkles::construction: Working on making information display as it is entered.

---
+++ resources/assets/js/components/LocationInputs.vue
@@ -0,0 +1,33 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                locations: [],
+                distanceApi: [],
+                distance: this.distanceApi.data.distance,
+                time: this.distanceApi.data.time
+            }
+        },
+        mounted() {
+            this.getLocations();
+        },
+        methods: {
+            getLocations() {
+                axios.get('/api/locations')
+                    .then(response => {
+                        this.locations  = response.data;
+                    })
+            },
+            getDistance(from, to) {
+                axios.get('/api/distance?from=' + from + '&to=' + to)
+                    .then(response => {
+                        this.distanceApi = response.data
+                    })
+            }
+        }
+    }
+</script>
--- routes/api.php
+++ routes/api.php
@@ -37,3 +37,31 @@ Route::group(['prefix' => '/locations', 'middleware' => 'auth:api'], function ()
     Route::post('/{location}')->middleware('scope:update-location'); // Update Location
     Route::delete('/{location}')->middleware('scope:delete-location'); // Delete Location
 });
+
+Route::get('/distance', function(Request $request) {
+    if($request->has('from') && $request->has('to')) {
+        try {
+            $origin = \App\Location::whereId($request->get('from'))->first();
+            $destination = \App\Location::whereId($request->get('to'))->first();
+            $httpClient = new HttpClient();
+            $response = $httpClient->get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' . $origin->address . '&destinations=' . $destination->address . '&key=' . config('services.google.key'));
+            if ($response->getStatusCode() < 200 || $response->getStatusCode() >= 400) {
+                // Return an error if we can not access google
+                return abort(500, 'We could not get the distance information from google. Did you put your google API key in the config? And did you enable the Distance Matrix API?');
+            }
+            // This is the raw data from Google converted to json
+            $rawData = (object)\GuzzleHttp\json_decode($response->getBody());
+
+            // This is the raw miles (meters to miles)
+            $cleanDistance = $rawData->rows[0]->elements[0]->distance->value / 1609.344;
+            // This is the seconds converted to minutes
+            $cleanTime = $rawData->rows[0]->elements[0]->duration->value / 60;
+            // Return the distance value
+            return response()->json(['code' => 200, 'message' => 'Distance returned', 'data' => ['distance' => (float)substr($cleanDistance, 0, 4), 'time' => (integer)$cleanTime]]);
+        } catch (Exception $e) {
+            return response()->json([code => 500, 'message' => $e->getMessage()], 500);
+        }
+    } else {
+        return response()->json(['code' => 400, 'message' => 'This endpoint needs a "to" parameter and a "from" parameter'], 500);
+    }
+})->middleware('auth:api');
