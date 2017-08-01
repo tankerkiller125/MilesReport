5569ac3b4d87fa82244d22e117081bf737a54859
tankerkiller125 <tankerkiller125@gmail.com>
2017-07-31 23:22:48 -0400

    Working on moving report generation to a queue for better performance and handling.

---
+++ app/Jobs/CreateReport.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Entry;
+use App\Location;
+use App\Notifications\SendUserReport as Notification;
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CreateReport implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user;
+
+    /**
+     * CreateReport constructor.
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // This crazy compare determines if the user wants the message sent today or if its too soon
+        if (Carbon::createFromFormat('Y-m-d H:i:s', $this->user->last_report)->addDays($this->user->report_schedule)->diffInDays(Carbon::now()) == 0) {
+            // Get all entries from last report sent to now
+            $sheet = $this->generateSheet($this->user->id, $this->user->name, $this->user->last_report);
+            $this->user->notify(new Notification($sheet));
+            $this->user->last_report = Carbon::now();
+            $this->user->save();
+        } else {
+            dd(false);
+        }
+    }
+
+    /**
+     * @param $userId
+     * @param $userName
+     * @param $lastReport
+     * @return string
+     */
+    private function generateSheet($userId, $userName, $lastReport)
+    {
+        $entries = Entry::whereUserId($userId)->whereDate('created_at', '>', $lastReport)->get()->map(function ($entry) {
+            $from = Location::whereId($entry->from)->first();
+            $to = Location::whereId($entry->to)->first();
+            return ['from' => $from->name, 'to' => $to->name, 'distance' => $entry->distance];
+        });
+        $sheet = \Excel::create('Miles-Report-' . str_replace(' ', '-', $userName) . '-' . Carbon::now()->format('F') . '-' . Carbon::now()->format('Y'), function ($excel) use ($entries) {
+            $excel->sheet('Miles', function ($sheet) use ($entries) {
+                $sheet->fromModel($entries);
+            });
+        })->store('csv', false, true);
+        return $sheet['file'];
+    }
+}
